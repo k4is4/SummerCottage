33c48adf70dad2cb7254d2bdad24f9bfd530a563
Kaisa Ranta <kaisa.ranta@avanade.com>
2024-12-03 12:16:30 +0200

    test backend url

--- Solution/cottage-ui/src/services/ItemService.ts
+++ Solution/cottage-ui/src/services/ItemService.ts
@@ -1,19 +1,26 @@
-import { AxiosError, AxiosResponse } from "axios";
-import Item from "../types/item";
-import apiClient from "./apiClient";
-import ItemFormData from "../types/itemFormData";
-import { ProblemDetails } from "../types/problemDetails";
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import Item from '../types/item';
+import apiClient from './apiClient';
+import ItemFormData from '../types/itemFormData';
+import { ProblemDetails } from '../types/problemDetails';
 
 class ItemService {
+	// async getItems(): Promise<Item[]> {
+	// 	return apiClient
+	// 		.get<Item[]>("/items")
+	// 		.then((response: AxiosResponse<Item[]>) => response.data);
+	// }
+
 	async getItems(): Promise<Item[]> {
-		return apiClient
-			.get<Item[]>("/items")
+		const backendUrl = 'https://app-cottage.azurewebsites.net/api';
+		return axios
+			.get<Item[]>(`${backendUrl}/items`)
 			.then((response: AxiosResponse<Item[]>) => response.data);
 	}
 
 	async addItem(item: ItemFormData): Promise<Item> {
 		return apiClient
-			.post<Item>("/items", item)
+			.post<Item>('/items', item)
 			.then((response: AxiosResponse<Item>) => response.data)
 			.catch(this.parseError<Item>);
 	}
@@ -32,9 +39,9 @@ class ItemService {
 	parseError<T>(errorResponse: AxiosError<ProblemDetails>): Promise<T> {
 		const errors = errorResponse.response?.data.errors;
 		if (errors) {
-			let errorsToDisplay: string = "";
+			let errorsToDisplay: string = '';
 			Object.keys(errors).forEach((key: string) => {
-				errorsToDisplay += errors[key] + "\n";
+				errorsToDisplay += errors[key] + '\n';
 			});
 			throw Error(errorsToDisplay);
 		} else {

6caee672a236317d5d35ccf45a68a97476fb883c
Kaisa Ranta <kaisa.ranta@avanade.com>
2024-12-04 15:15:33 +0200

    Remove auth config

--- Solution/cottage-ui/src/index.tsx
+++ Solution/cottage-ui/src/index.tsx
@@ -6,26 +6,14 @@ import './custom.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 
-// import { PublicClientApplication } from '@azure/msal-browser';
-// import { MsalProvider } from '@azure/msal-react';
-// import { msalConfig } from './authConfig';
-
-/**
- * Initialize a PublicClientApplication instance which is provided to the MsalProvider component
- * We recommend initializing this outside of your root component to ensure it is not re-initialized on re-renders
- */
-// const msalInstance = new PublicClientApplication(msalConfig);
-
 const root = document.getElementById('root');
 if (root !== null) {
 	const appRoot = createRoot(root);
 	appRoot.render(
 		<React.StrictMode>
-			{/* <MsalProvider instance={msalInstance}> */}
 			<BrowserRouter>
 				<App />
 			</BrowserRouter>
-			{/* </MsalProvider> */}
 		</React.StrictMode>
 	);
 }

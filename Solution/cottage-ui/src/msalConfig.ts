137255504b9bf76ae36078a4bcb3b1282a7805ae
Kaisa Ranta <kaisa.ranta@avanade.com>
2024-12-05 13:54:50 +0200

    modified redirectUri

--- Solution/cottage-ui/src/msalConfig.ts
+++ Solution/cottage-ui/src/msalConfig.ts
@@ -5,7 +5,8 @@ const msalConfig = {
 		clientId: '111726ea-c3c1-4d90-8ec4-f3a2595c3bd0',
 		authority:
 			'https://login.microsoftonline.com/bf05f699-9934-4606-9238-1a93f805568f', // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
-		redirectUri: 'https://app-cottage.azurewebsites.net/',
+		redirectUri:
+			'https://icy-pond-0f1e60f03.4.azurestaticapps.net/.auth/login/aad/callback',
 	},
 };
 

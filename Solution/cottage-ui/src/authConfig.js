a8110022fab42e3131ba9f93ca3fa056fd5e9c6d
Kaisa Ranta <kaisa.ranta@avanade.com>
2024-12-09 13:29:10 +0200

    modified redirect uri

--- Solution/cottage-ui/src/authConfig.js
+++ Solution/cottage-ui/src/authConfig.js
@@ -15,8 +15,7 @@ export const msalConfig = {
 		clientId: '111726ea-c3c1-4d90-8ec4-f3a2595c3bd0',
 		authority:
 			'https://login.microsoftonline.com/bf05f699-9934-4606-9238-1a93f805568f', // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
-		redirectUri:
-			'https://icy-pond-0f1e60f03.4.azurestaticapps.net/.auth/login/aad/callback',
+		redirectUri: 'https://icy-pond-0f1e60f03.4.azurestaticapps.net',
 	},
 	cache: {
 		cacheLocation: 'sessionStorage', // This configures where your cache will be stored
